fea45c22e9d295c570a30cc7b9b1231a738d9ab4
Stefan Dili <stefan.dili.021@gmail.com>
2020-06-12 18:28:18 +0200

    resolving weird rubocop issues

--- Gemfile
+++ Gemfile
@@ -34,8 +34,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails', '~> 5.2'
-  gem 'sqlite3', '~> 1.4'
   gem 'faker'
+  gem 'sqlite3', '~> 1.4'
 end
 
 group :development do
--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -10,21 +10,21 @@ RSpec.feature 'Sessions', type: :feature do
 
   it 'Signs the user with valid username' do
     visit '/login'
-    fill_in 'session_username',	with: user.username
+    fill_in 'session_username', with: user.username
     click_button 'Log In'
     expect(page).to have_content('Factory Bot')
   end
 
   it "Doesn't sign in with an invalid username" do
     visit '/login'
-    fill_in 'session_username',	with: 'Not valid username'
+    fill_in 'session_username', with: 'Not valid username'
     click_button 'Log In'
     expect(page).to have_content('Invalid username')
   end
 
   it 'Logs the user out of the session' do
     visit '/login'
-    fill_in 'session_username',	with: user.username
+    fill_in 'session_username', with: user.username
     click_button 'Log In'
     click_on 'Log out'
     expect(page).to have_content("Don't have an account? Sign up now!")
